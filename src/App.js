d53c8f613a0f0ecac1884cd8f8af21e62add85a1
MarkiianDubyna <dybuna.mark@gmail.com>
2021-04-05 19:17:49 +0300

    added branch "home-work-4"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useParams,
+  useRouteMatch,
+  useHistory,
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/posts">Posts</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/posts" exact>
+              <Posts />
+            </Route>
+            <Route path="/posts/:id">
+              <PostDetails />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Posts() {
+  const [posts, setPosts] = useState([])
+  const fetchData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const json = await response.json();
+    setPosts(json);
+  }
+  useEffect(() => {
+    fetchData();
+  }, [])
+  return (
+      <div>
+        <ul>
+          {posts.map(post =>
+              <Link to={`/posts/${post.id}`}><li>{post.title} - {post.id}</li></Link>
+          )}
+        </ul>
+      </div>
+  );
+}
+function PostDetails() {
+  const [post, setPost] = useState([]);
+  const match = useRouteMatch();
+  const {id} = useParams();
+  const history = useHistory();
+  const fetchData = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const json = await response.json();
+    setPost(json);
+  }
+  useEffect(() => {
+    fetchData();
+  }, [])
+  return (
+      <div>
+        <h2>Post details</h2>
+        {post && <> <h3>{post.title}</h3> <p>{post.body}</p></>}
+
+        <button onClick={() => history.push(`/posts/${+id +1}`)}>Go to next post</button>
+      </div>
+  );
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
